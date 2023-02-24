8979fa78505f98f9baf18b3284479572f50af665
Luc Döbereiner <luc.doebereiner@gmail.com>
2023-02-24 17:34:02 +0100

    cam stuff

--- src/main.cpp
+++ src/main.cpp
@@ -18,30 +18,33 @@ public:
 		// webcam.videoSettings();
 
 		// Initialize the mesh
+		// mesh.setMode(OF_PRIMITIVE_LINES);
 		mesh.setMode(OF_PRIMITIVE_TRIANGLES);
 		mesh.enableColors();
+		mesh.enableIndices();
 
 		// Create the vertices
-		int w = webcam.getWidth();
-		int h = webcam.getHeight();
-		for (int y = 0; y < h; y++)
+		width = 40;	 // webcam.getWidth();
+		height = 40; // webcam.getHeight();
+		dist = 20;
+		for (int y = 0; y < height; y++)
 		{
-			for (int x = 0; x < w; x++)
+			for (int x = 0; x < width; x++)
 			{
-				mesh.addVertex(ofVec3f(x - w / 2, y - h / 2, 0));
+				mesh.addVertex(ofVec3f(x * dist, y * dist, 0));
 				mesh.addColor(ofColor(255, 255, 255));
 			}
 		}
 
 		// Create the indices
-		for (int y = 0; y < h - 1; y++)
+		for (int y = 0; y < height - 1; y++)
 		{
-			for (int x = 0; x < w - 1; x++)
+			for (int x = 0; x < width - 1; x++)
 			{
-				int i1 = x + y * w;
-				int i2 = (x + 1) + y * w;
-				int i3 = x + (y + 1) * w;
-				int i4 = (x + 1) + (y + 1) * w;
+				int i1 = x + y * width;
+				int i2 = (x + 1) + y * width;
+				int i3 = x + (y + 1) * width;
+				int i4 = (x + 1) + (y + 1) * width;
 				mesh.addIndex(i1);
 				mesh.addIndex(i2);
 				mesh.addIndex(i3);
@@ -57,8 +60,30 @@ public:
 		webcam.update();
 		if (webcam.isFrameNew())
 		{
-			// Update the mesh vertices based on the webcam pixels
-			int w = webcam.getWidth();
+			ofPixels pixs = webcam.getPixels();
+			pixs.resize(width, height, OF_INTERPOLATE_BICUBIC);
+
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					int i = x + y * width;
+					ofVec3f pos = mesh.getVertex(i);
+					int bright = pixs.getColor(x, y).getBrightness();
+					pos.z = ofMap(bright, 0, 255, -100, 100);
+					mesh.setVertex(i, pos);
+					mesh.setColor(i, pixs.getColor(x, y));
+					// mesh.setColor(i, ofColor(bright, bright, bright));
+				}
+			}
+
+			// pixs.resize(240, 240);
+			// image.setFromPixels(pixs);
+			//  webcam.update();
+			//   webcam.getPixels().resize(640, 480);
+			//   webcam.update();
+			//    Update the mesh vertices based on the webcam pixels
+			/*int w = webcam.getWidth();
 			int h = webcam.getHeight();
 			for (int y = 0; y < h; y++)
 			{
@@ -71,6 +96,7 @@ public:
 					mesh.setColor(i, webcam.getPixels().getColor(x, y));
 				}
 			}
+			*/
 		}
 	}
 
@@ -80,21 +106,39 @@ public:
 		// ofPushMatrix();
 		// ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2, 0);
 		// ofRotateX(180);
+		// webcam.draw(0, 0);
+
+		// ofPushMatrix();
+		// ofTranslate(640, 0, 0);
+		easyCam.begin();
+		ofPushMatrix();
+		ofTranslate(-ofGetWidth() / 2, -ofGetHeight() / 2);
+		mesh.draw();
+		ofPopMatrix();
+		easyCam.end();
+
 		// mesh.draw();
-		webcam.draw(0, 0);
 		// ofPopMatrix();
+
+		// image.draw(640, 0);
+		//  ofPopMatrix();
 		ofDisableDepthTest();
 	}
 
 private:
 	ofVideoGrabber webcam;
 	ofMesh mesh;
+	ofImage image;
+	ofEasyCam easyCam;
+	int width;
+	int height;
+	int dist;
 };
 
 int main()
 {
 	std::cout << "starting" << std::endl;
-	ofSetupOpenGL(640, 480, OF_WINDOW);
+	ofSetupOpenGL(1000, 480, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
 /*
